8f6f9d95dac97407bfd456d23d4dc8565330b259
andrii2706 <andriipas200017@gmail.com>
2025-03-03 23:07:11 +0200

    add some logic for form

--- src/app/pages/dashboard/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard/dashboard.component.ts
@@ -1,19 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { ClearObservable } from '../../../shared/clasess/ClearObservable';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [MatButtonModule, ReactiveFormsModule, MatInputModule, MatDatepickerModule],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent extends ClearObservable implements OnInit {
+
+  todoForm: FormGroup
+
   constructor() {
       super();
   }
   ngOnInit(): void {
-    
+    this.initForm()
   }
-  clearForm() {}
-  submitForm() {}
+  initForm(){
+    this.todoForm = new FormGroup({
+        nameOfTask: new FormControl("", Validators.required),
+        starttimeOfTask: new FormControl("", Validators.required),
+        endtimeOfTask: new FormControl("", Validators.required),
+        taskStatus: new FormControl("Draft", Validators.required),
+    })
+  }
+  clearForm() {
+    this.todoForm.reset();
+  }
+  submitForm() {
+    console.log(this.todoForm.value);
+  }
 }
