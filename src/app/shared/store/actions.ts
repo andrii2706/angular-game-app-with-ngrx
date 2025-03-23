5717ca878029106dac15b48c1841592d12e54f9a
andrii2706 <andriipas200017@gmail.com>
2025-03-23 15:52:13 +0200

    add some changes

---
+++ src/app/shared/store/actions.ts
@@ -0,0 +1,9 @@
+import { createAction, props } from "@ngrx/store";
+
+export const addTodo = createAction('Add Todo task', props<{name : string, starttimeOfTask: string, endtimeOfTask: string, taskStatus:string}>());
+export const loadTodos = createAction('Load Todo tasks', props<{name : string, starttimeOfTask: string, endtimeOfTask: string, taskStatus:string}>());
+export const loadTodosSuccess = createAction('Load Todo tasks success');
+export const loadTodosError = createAction('Load Todo tasks error');
+export const getGamesFromBe = createAction ("Get game data", props<any>)
+export const errorGames = createAction ("Error to get games")
+export const succesGames = createAction ("Success get games")
